424e09e8ac2ebc344ccce2a91b03da5fc515d35c
lddsb <luodi0128@gmail.com>
2021-02-13 08:12:59 +0800

    feat: camelcase to underscore

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,9 +10,68 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/urfave/cli/v2"
 )
+
+// Buffer bytes.Buffer pointer
+type Buffer struct {
+	*bytes.Buffer
+}
+
+// NewBuffer bytes.Buffer
+func NewBuffer() *Buffer {
+	return &Buffer{Buffer: new(bytes.Buffer)}
+}
+
+// Append bytes.Buffer append
+func (b *Buffer) Append(i interface{}) *Buffer {
+	switch val := i.(type) {
+	case int:
+		b.append(strconv.Itoa(val))
+	case int64:
+		b.append(strconv.FormatInt(val, 10))
+	case uint:
+		b.append(strconv.FormatUint(uint64(val), 10))
+	case uint64:
+		b.append(strconv.FormatUint(val, 10))
+	case string:
+		b.append(val)
+	case []byte:
+		b.Write(val)
+	case rune:
+		b.WriteRune(val)
+	}
+	return b
+}
+
+func (b *Buffer) append(s string) *Buffer {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("*****内存不够了！******")
+		}
+	}()
+	b.WriteString(s)
+	return b
+}
+
+// UnderscoreName camelcase to underscore
+func underscoreName(name string) string {
+	buffer := NewBuffer()
+	for i, r := range name {
+		if unicode.IsUpper(r) {
+			if i != 0 {
+				buffer.Append('_')
+			}
+			buffer.Append(unicode.ToLower(r))
+		} else {
+			buffer.Append(r)
+		}
+	}
+
+	return buffer.String()
+}
 
 func main() {
 	app := cli.NewApp()
@@ -141,14 +201,15 @@ func jsonFile(source string, outfile string) {
 // jsonRecursive recursively traverse the JSON file
 func jsonRecursive(mp map[string]interface{}) map[string]interface{} {
 	for k, v := range mp {
-		env := os.Getenv(strings.ToUpper(k))
+		env := os.Getenv(strings.ToUpper(underscoreName(k)))
+		if env == "" {
+			continue
+		}
+
 		switch x := v.(type) {
 		case map[string]interface{}:
 			mp[k] = jsonRecursive(x)
 		case float64:
-			if env == "" {
-				env = "0"
-			}
 			envFloat, err := strconv.ParseFloat(env, 64)
 			if err != nil {
 				log.Fatalln(err)
